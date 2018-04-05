a0a3817a8c634e007c2513e405334cc2d695bcb0
Felipe Vieira <flpchapola@hotmail.com>
2018-04-04 23:24:58 -0300

    Adicionado as imagens locais no build do webpack com a ajuda de um helper, criado o fundo e colocado a logo

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,74 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img class="logo" src="./assets/img/star-wars-logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-};
+  export default {
+    name: 'App',
+    methods: {
+      backgroundStars() {
+        // Sets the number of stars we wish to display
+        const numStars = 350;
+        // For every star we want to display
+        for (let i = 0; i < numStars; i++) {
+          let star = document.createElement("div");
+          star.className = "star";
+          var xy = getRandomPosition();
+          star.style.top = xy[0] + 'px';
+          star.style.left = xy[1] + 'px';
+          document.body.append(star);
+        }
+        // Gets random x, y values based on the size of the container
+        function getRandomPosition() {
+          var y = window.innerWidth;
+          var x = window.innerHeight;
+          var randomX = Math.floor(Math.random() * x);
+          var randomY = Math.floor(Math.random() * y);
+          return [randomX, randomY];
+        }
+      }
+    },
+    mounted: function() {
+      this.backgroundStars();
+    }
+  };
 </script>
 
 <style lang="scss">
- @import "~vue-material/dist/theme/engine"; // Import the theme engine
-
+  @import "~vue-material/dist/theme/engine"; // Import the theme engine
   @include md-register-theme("default", ( primary: md-get-palette-color(blue, A200), // The primary color of your application
   accent: md-get-palette-color(red, A200), // The accent or secondary color
   theme: dark // This can be dark or light
   ));
   @import "~vue-material/dist/theme/all"; // Apply the theme
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
+  html {
+    background-color: black!important;
+    > body {
+      margin: 0;
+      overflow-x: hidden;
+      > #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+        .star {
+          position: absolute;
+          width: 1px;
+          height: 1px;
+          background-color: white;
+        }
+        >.logo {
+          display: block;
+          margin: 0 auto;
+          width: 320px;
+          margin-bottom: 50px;
+        }
+      }
+    }
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,97 +1,99 @@
 <template>
   <div class="md-layout md-gutter md-alignment-top-center">
-     <div class="md-layout-item"  v-for="person of peopleWithImage">
+    <div class="md-layout-item" v-for="person of peopleWithImage">
       <md-card>
-        <md-card-media-cover md-text-scrim>
-          <md-card-media md-ratio="16:9">
-            <img src="@/assets/people/1.jpg" alt="Skyscraper">
-          </md-card-media>
-
-          <md-card-area>
-            <md-card-header>
-              <span class="md-title">Ano de nascimento : {{person.birth_year}}</span>
-              <span class="md-subhead">Sexo: {{person.gender}}</span>
-              <span class="md-subhead">Nome: {{person.name}}</span>
-            </md-card-header>
-
-            <md-card-actions>
+        <md-card-media>
+          <img :src="getPic(person.img)" alt="People">
+        </md-card-media>
+        <md-card-header>
+          <div class="md-title">Nascimento: {{person.birth_year}}</div>
+          <div class="md-subhead">Sexo: {{person.gender}}</div>
+        </md-card-header>
+        <md-card-expand>
+          <md-card-actions md-alignment="space-between">
+            <div>
               <md-button>Action</md-button>
-              <md-button>Action</md-button>
-            </md-card-actions>
-          </md-card-area>
-        </md-card-media-cover>
+            </div>
+            <md-card-expand-trigger>
+              <md-button>Learn more</md-button>
+            </md-card-expand-trigger>
+          </md-card-actions>
+          <md-card-expand-content>
+            <md-card-content>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+            </md-card-content>
+          </md-card-expand-content>
+        </md-card-expand>
       </md-card>
-
     </div>
   </div>
 </template>
+
 <script>
-import axios from 'axios';
-
-export default {
-  name: 'Home',
-  data() {
-    return {
-
-      people: [],
-      nextPage: [],
-      previousPage: [],
-      images: [
-          {
+  import axios from 'axios';
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        people: [],
+        nextPage: [],
+        previousPage: [],
+        images: [{
             id: 1,
-            img: "./assets/people/1.jpg"
+            img: "1.jpg"
           },
           {
             id: 2,
-            img:"~/assets/people/2.jpg"
+            img: "2.jpg"
           },
           {
             id: 3,
-            img:"@/assets/people/3.jpg"
+            img: "3.jpg"
           },
           {
             id: 4,
-            img:"../img/people/4.jpg"
+            img: "4.jpg"
           },
           {
             id: 5,
-            img: "../img/people/5.jpg"
+            img: "5.jpg"
           },
           {
             id: 6,
-            img:"../assets/people/6.jpg"
+            img: "6.jpg"
           },
           {
             id: 7,
-            img:"../assets/people/7.jpg"
+            img: "7.jpg"
           },
           {
             id: 8,
-            img:"../assets/people/8.jpg"
+            img: "8.jpg"
           },
           {
             id: 9,
-            img:"../assets/people/9.jpg"
+            img: "9.jpg"
           },
           {
             id: 10,
-            img:"../assets/people/10.jpg"
+            img: "10.jpg"
           }
         ]
-    }
-  },
-  methods: {
-  // mountImagePath: function(img){
-  //         return require('@/assets/people/'+ img + '.jpg')
-  //       },
-  },
+      }
+    },
+    methods: {
+      // mountImagePath: function(img){
+      //         return require('@/assets/people/'+ img + '.jpg')
+      //       },
+      //   getImgUrl(pic) {
+      //     return require('../assets/'+pic)
+      // }
+      getPic(person) {
+        return require('@/assets/img/people/' + person)
+      }
+    },
     computed: {
-
-
-
       peopleWithImage() {
-
-
         // for (let i= 0; i < this.people.length; i++) {
         // let idStarWarsApi = this.people[i].url.match(/\/([^\/]+)\/?$/)[1];
         //  for (let i= 0; i < images.length; i++) {
@@ -102,17 +104,16 @@ export default {
         //     }
         //   }
         // }
-
-
         let peopleFinal = [];
-        for (let i= 0; i < this.images.length; i++) {
+        for (let i = 0; i < this.images.length; i++) {
           let imagesId = this.images[i].id;
           console.log(imagesId)
-          for (let i= 0; i < this.people.length; i++) {
+          for (let i = 0; i < this.people.length; i++) {
             let idStarWarsApi = this.people[i].url.match(/\/([^\/]+)\/?$/)[1];
-
-            if(imagesId === parseInt(idStarWarsApi)){
-              let combined = { ...this.people[i], ...this.images[i] }
+            if (imagesId === parseInt(idStarWarsApi)) {
+              let combined = { ...this.people[i],
+                ...this.images[i]
+              }
               peopleFinal.push(combined)
               // console.log(combined);
               // let x = this.people[i].push(images[i]);
@@ -120,38 +121,41 @@ export default {
             }
           }
         }
-
-      console.log("CONSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSOLE"+peopleFinal);
-      return peopleFinal;
+        console.log("CONSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSOLE" + peopleFinal);
+        return peopleFinal;
       }
     },
-   created() {
-    axios.get(`https://cors-anywhere.herokuapp.com/https://swapi.co/api/people`, {
-      headers: {
-
-
-                baseURL: 'http://localhost:8080',
-                timeout: 10000,
-                withCredentials: false
-      }
+    created() {
+      axios.get(`https://cors-anywhere.herokuapp.com/https://swapi.co/api/people`, {
+          headers: {
+            baseURL: 'http://localhost:8080',
+            timeout: 10000,
+            withCredentials: false
+          }
         })
-    .then(response => {
-      this.people = response.data.results;
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
+        .then(response => {
+          this.people = response.data.results;
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+    }
   }
-
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
-.md-card {
+  .md-card {
     width: 320px;
     margin: 4px;
     display: inline-block;
     vertical-align: top;
+    >.md-card-media{
+ height: 180px!important;
+    overflow: hidden;
+    }
   }
+
+
+
 </style>
